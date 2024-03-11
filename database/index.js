d2e28603423e5d0111c7772e6cee3b32f3addaa7
Jackson Soriano <jackson.masters.it@gmail.com>
2024-03-11 22:11:24 +0800

    db config updated

--- database/index.js
+++ database/index.js
@@ -10,5 +10,9 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 });
+pool.getConnection((err, conn) => {
+    if(err) console.log(err)
+    console.log("Connected successfully")
+})
 
 module.exports = pool.promise();
