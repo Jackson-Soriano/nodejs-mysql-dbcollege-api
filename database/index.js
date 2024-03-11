4fdd66394990006d0be0240111bfa9e24d1ee154
Jackson Soriano <jackson.masters.it@gmail.com>
2024-03-11 21:49:08 +0800

    .env updated

--- database/index.js
+++ database/index.js
@@ -5,7 +5,10 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = pool.promise();
