dd13d2a0b5f9f530a32947f06bcca43824966dc7
Jackson Soriano <jackson.masters.it@gmail.com>
2024-03-11 22:55:49 +0800

    fixed env database config

--- database/index.js
+++ database/index.js
@@ -6,13 +6,6 @@ const pool = mysql.createPool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-pool.getConnection((err, conn) => {
-    if(err) console.log(err)
-    console.log("Connected successfully")
-})
+    });
 
 module.exports = pool.promise();
